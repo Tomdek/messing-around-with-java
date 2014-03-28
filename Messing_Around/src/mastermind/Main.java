1cf4736284fd619e6717800a9e88f168f47dde1f
Thomas Dekker <tomdek@upcmail.nl>
2014-03-28 13:29:32 +0100

    Added: Start message and rules, also code overview per turn.

--- Messing_Around/src/mastermind/Main.java
+++ Messing_Around/src/mastermind/Main.java
@@ -14,14 +14,22 @@ public class Main {
 	public static final int MAGENTA = 7;
 	public static final int BLUE = 8;
 	
-	Scanner sc = new Scanner(System.in);
+	private Scanner sc = new Scanner(System.in);
+	private String overview = "Overview of colours: red, white, yellow, green, purple, orange, magenta, blue.\n";
 	
 	private void gameOver(boolean hasWon) {
-		//TODO game over screen: if haswon==true then player won!
+		if (hasWon) {
+			System.out.print(" ---------------- \n|    You won!    |\n|Congratulations!|\n ---------------- \n");
+		} else {
+			System.out.print(" ---------------- \n|   Game Over    |\n ---------------- \n");
+		}
+		System.out.print("Press Enter to play again.");
+		sc.nextLine();
+		start();
 	}
 	
 	private void visualize(Code[] attempts, int[][] feedback) {
-		//TODO visualize it somehow
+		//TODO visualize the entire board (including all past turns)
 	}
 	
 	private Code askAttempt() {
@@ -29,6 +37,7 @@ public class Main {
 		boolean validCode = false;
 
 		while(!validCode) {
+			System.out.print(overview);
 			System.out.print("Your code: ");
 			String input = sc.nextLine();
 			char[] tokens = input.toLowerCase().trim().toCharArray();
@@ -51,7 +60,24 @@ public class Main {
 		return result;
 	}
 	
+	void printStartingMessage() {
+		String s1 = "Welcome to Text-Mastermind. The rules are simple:\n";
+		String s2 = "It is your job to break the secret colour-code within " + MAX_NR_OF_TURNS + " turns.\n";
+		String s3 = "To do this, you enter a sequence of 4 colours and press return.\n";
+		String s4 = "Feedback will then be given through the colours Red and White.\n";
+		String s5 = "White means a colour is correct, red means a colour is correct and correctly placed.\n";
+		String s6 = "The order of the feedback does necesserily not correspond to the order of the code.\n";
+		String s7 = "The total number of colours is 8 and each colour can only exist once in a code.\n";
+		String s8 = "To enter a code simply type the first letter of each colour.\n";
+		String s9 = "EXAMPLE: to enter \"Red Green Blue White\" you type: r g b w\n";
+		String s10 = "GOOD LUCK!\n";
+		String br = "\n";
+		System.out.print(s1 + s2 + s3 + s4 + s5 + s6 + s7 + br + s8 + s9 + s10);
+	}
+	
 	void start() {
+		printStartingMessage();
+		
 		Code secretCode = new Code(true);
 		Code[] attempts = new Code[MAX_NR_OF_TURNS];
 		int[][] feedback = new int[MAX_NR_OF_TURNS][4];
@@ -80,7 +106,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		new Main().start();
-		
 
 	}
 
