3ce7f39cc4de0293f1fb33e536f165c4b5a3c1cc
Thomas Dekker <tomdek@upcmail.nl>
2014-03-28 11:55:21 +0100

    askAttempt() function now exists (untested) in conjunction with the way the Code class works, should be robust enough to accept any input and take the first 4 valid letters as code, or not find 4 valid letters and ask for new input.

--- Messing_Around/src/mastermind/Main.java
+++ Messing_Around/src/mastermind/Main.java
@@ -1,5 +1,7 @@
 package mastermind;
 
+import java.util.Scanner;
+
 public class Main {
 	//written by Thomas Dekker
 	public static final int MAX_NR_OF_TURNS = 10;
@@ -12,6 +14,8 @@ public class Main {
 	public static final int MAGENTA = 7;
 	public static final int BLUE = 8;
 	
+	Scanner sc = new Scanner(System.in);
+	
 	private void gameOver(boolean hasWon) {
 		//TODO game over screen: if haswon==true then player won!
 	}
@@ -21,13 +25,34 @@ public class Main {
 	}
 	
 	private Code askAttempt() {
-		Code result = new Code();
-		//TODO Get user input and convert to Code object
+		Code result = new Code(false);
+		boolean validCode = false;
+
+		while(!validCode) {
+			System.out.print("Your code: ");
+			String input = sc.nextLine();
+			char[] tokens = input.toLowerCase().trim().toCharArray();
+
+			for (int i = 0; i < tokens.length; i++) {
+				switch (tokens[i]) {
+				case 'r': result.setNextColour(RED); break;
+				case 'w': result.setNextColour(WHITE); break;
+				case 'y': result.setNextColour(YELLOW); break;
+				case 'g': result.setNextColour(GREEN); break;
+				case 'p': result.setNextColour(PURPLE); break;
+				case 'o': result.setNextColour(ORANGE); break;
+				case 'm': result.setNextColour(MAGENTA); break;
+				case 'b': result.setNextColour(BLUE); break;
+				default: break;
+				}
+			}
+			validCode = result.getLength() == 4;
+		}
 		return result;
 	}
 	
 	void start() {
-		Code secretCode = new Code();
+		Code secretCode = new Code(true);
 		Code[] attempts = new Code[MAX_NR_OF_TURNS];
 		int[][] feedback = new int[MAX_NR_OF_TURNS][4];
 		int turn = 0;
